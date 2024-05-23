7b819d1e233d2753560e83dbada23a5a0422e3d7
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-23 20:06:51 +0330

    added unit test for GetDB and Init functions

--- db/db_test.go
+++ db/db_test.go
@@ -3,10 +3,14 @@ package db
 import (
 	"github.com/stretchr/testify/require"
 	"testing"
+	"url-shortener/config"
 )
 
 // TestGetDB tests GetDB function
 func TestGetDB(t *testing.T) {
+	conf := config.GetConfig("config", "../config", "json")
+	Init(conf.DatabaseAddress())
+
 	database := GetDB()
 	require.NotEmpty(t, database)
 }
