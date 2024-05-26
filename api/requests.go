51d90be8918fad02a62b59d6b6055fcf7a305113
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-26 23:17:17 +0330

    defined RedirectShortUrl

--- api/requests.go
+++ api/requests.go
@@ -5,3 +5,9 @@ type ShortenUrlRequest struct {
 	// URL to be shortened
 	URL string `json:"url" binding:"url"`
 }
+
+// RedirectShortUrl represents a request to be redirected to the original url
+type RedirectShortUrl struct {
+	// ShortUrl short url
+	ShortUrl string `uri:"short_url" binding:"required,len=6"`
+}
