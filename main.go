1c659d6e752cd23be2fcbed6c1a5c1ba19d77eae
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-26 23:21:27 +0330

    added logic to start the service

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"url-shortener/api"
+	"url-shortener/config"
+	"url-shortener/db"
+)
 
+func main() {
+	// load configs
+	configs := config.GetConfig("config", "./config", "json")
+
+	// init database
+	db.Init(configs.DatabaseAddress())
+
+	// get the singleton server instance
+	api.Init(db.GetDB(), configs.ServerAddress())
+	server := api.GetServer()
+
+	// start the server
+	server.StartServer()
 }
