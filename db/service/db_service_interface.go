ae49ebaba7871f3b3d0c6ebbc889a4b33f7a4802
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-26 22:22:54 +0330

    added GetOriginalUrl method to DBService interface

--- db/service/db_service_interface.go
+++ db/service/db_service_interface.go
@@ -3,6 +3,9 @@ package service
 // DBService defines methods to interact with the database
 type DBService interface {
 	// SaveUrl creates a models.Url instance from (req SaveUrlRequest) and saves it into the database
-	// returns an error instance if any
+	// returns an error if any
 	SaveUrl(req *SaveUrlRequest) (*SaveUrlResponse, error)
+	// GetOriginalUrl returns the original url of a shortened url from the database
+	// returns an error if any
+	GetOriginalUrl(req *GetOriginalUrlRequest) (*GetOriginalUrlResponse, error)
 }
