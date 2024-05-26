2238c6667cf175adf2af3fa73e768449f6fdcb6b
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-26 23:19:15 +0330

    added redirectShortUrl handler for "/redirect/:short_url" route

--- api/handlers.go
+++ api/handlers.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"net/http"
 	"url-shortener/db/service"
 	"url-shortener/util"
@@ -35,6 +37,32 @@ func (s *server) shortenUrl(context *gin.Context) {
 
 	// send response to the client
 	context.JSON(http.StatusOK, response)
+}
+
+// redirectShortUrl redirects client to the original url of the given short url
+func (s *server) redirectShortUrl(context *gin.Context) {
+	var req RedirectShortUrl
+
+	// get the request uri
+	if err := context.ShouldBindUri(&req); err != nil {
+		context.JSON(http.StatusBadRequest, errResponse(err))
+		return
+	}
+
+	// retrieve the original url of the short url
+	res, err := s.dbService.GetOriginalUrl(service.NewGetOriginalUrlRequest(req.ShortUrl))
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			context.JSON(http.StatusNotFound, "url does not exist")
+			return
+		}
+
+		context.JSON(http.StatusInternalServerError, errResponse(err))
+		return
+	}
+
+	// redirect the client to the original url
+	context.Redirect(http.StatusPermanentRedirect, res.OriginalUrl())
 }
 
 // errResponse converts a error instance to a gin.H instance
