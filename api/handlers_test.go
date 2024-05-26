86ac17120e642b7f1b9068938f1716f8b3a5e3f3
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-26 20:54:03 +0330

    updated SaveUrl handler tests

--- api/handlers_test.go
+++ api/handlers_test.go
@@ -2,32 +2,77 @@ package api
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	mockdb "url-shortener/db/mock"
+	"url-shortener/db/models"
+	"url-shortener/db/service"
+	"url-shortener/util"
 )
+
+// //////////////////////////////////////////////////////////////
+// saveUrlRequestMatcher is a custom gomock matcher
+type saveUrlRequestMatcher struct {
+	originalUrl string
+}
+
+func (s saveUrlRequestMatcher) Matches(x any) bool {
+	inputSession, ok := x.(*service.SaveUrlRequest)
+	if !ok {
+		return false
+	}
+
+	return s.originalUrl == inputSession.OriginalUrl()
+}
+
+func (s saveUrlRequestMatcher) String() string {
+	return fmt.Sprintf("is equal to %v (%T)", s.originalUrl, s.originalUrl)
+}
+
+func newSaveUrlRequestMatcher(originalUrl string) gomock.Matcher {
+	return saveUrlRequestMatcher{
+		originalUrl: originalUrl,
+	}
+}
+
+////////////////////////////////////////////////////////////////
 
 // TestShortenUrl tests shortenUrl handler function
 func TestShortenUrl(t *testing.T) {
-	// get the singleton server instance
-	server := GetServer()
+	// assume we have this record in the database in the urls table
+	urlRecord := models.Url{
+		OriginalUrl: "http://example.test",
+		ShortUrl:    util.GenerateShortUrl(),
+	}
 
 	testCases := []struct {
 		name          string
 		req           ShortenUrlRequest
-		checkResponse func(t *testing.T, req ShortenUrlRequest, recorder *httptest.ResponseRecorder)
+		buildStubs    func(dbService *mockdb.MockDBService, req ShortenUrlRequest)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
 	}{
 		{
 			name: "OK",
-			req:  ShortenUrlRequest{URL: "https://example.test"},
-			checkResponse: func(t *testing.T, req ShortenUrlRequest, recorder *httptest.ResponseRecorder) {
+			req:  ShortenUrlRequest{URL: urlRecord.OriginalUrl},
+			buildStubs: func(dbService *mockdb.MockDBService, req ShortenUrlRequest) {
+				dbService.
+					EXPECT().
+					SaveUrl(newSaveUrlRequestMatcher(req.URL)).
+					Return(service.NewSaveUrlResponse(urlRecord.OriginalUrl, urlRecord.ShortUrl), nil).
+					Times(1)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
 				// check status code
 				require.Equal(t, http.StatusOK, recorder.Code)
 
-				// convert body from json to apiio.ShortenUrlResponse
+				// convert body from json to ShortenUrlResponse
 				var response ShortenUrlResponse
 
 				jsonBody, err := io.ReadAll(recorder.Body)
@@ -37,22 +82,53 @@ func TestShortenUrl(t *testing.T) {
 				require.NoError(t, err)
 
 				// check response body
-				require.Equal(t, req.URL, response.OriginalUrl)
-				require.NotEmpty(t, response.ShortUrl)
+				require.Equal(t, urlRecord.OriginalUrl, response.OriginalUrl)
+				require.Equal(t, urlRecord.ShortUrl, response.ShortUrl)
 			},
 		},
 		{
 			name: "BadRequest",
 			req:  ShortenUrlRequest{URL: "example.test"}, // invalid url
-			checkResponse: func(t *testing.T, req ShortenUrlRequest, recorder *httptest.ResponseRecorder) {
+			buildStubs: func(dbService *mockdb.MockDBService, req ShortenUrlRequest) {
+				dbService.EXPECT().SaveUrl(gomock.Any()).Times(0)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
 				// check status code
 				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			name: "InternalServerError",
+			req:  ShortenUrlRequest{URL: urlRecord.OriginalUrl},
+			buildStubs: func(dbService *mockdb.MockDBService, req ShortenUrlRequest) {
+				dbService.
+					EXPECT().
+					SaveUrl(newSaveUrlRequestMatcher(req.URL)).
+					Return(nil, sql.ErrConnDone).
+					Times(1)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				// check status code
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
 			},
 		},
 	}
 
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
+			// get a new gomock controller
+			controller := gomock.NewController(t)
+			defer controller.Finish()
+
+			// get an instance of the MockDBService (mock implementation of service.DBService interface)
+			dbService := mockdb.NewMockDBService(controller)
+
+			// build stubs
+			testCase.buildStubs(dbService, testCase.req)
+
+			// get a new test server
+			server := newTestServer(dbService)
+
 			// convert request body to json
 			requestBody, err := json.Marshal(testCase.req)
 			require.NoError(t, err)
@@ -65,7 +141,7 @@ func TestShortenUrl(t *testing.T) {
 
 			server.router.ServeHTTP(recorder, httpReq)
 
-			testCase.checkResponse(t, testCase.req, recorder)
+			testCase.checkResponse(t, recorder)
 		})
 	}
 }
